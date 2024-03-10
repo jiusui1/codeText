2a984a2fd1514649795b3d8f9cf53b1d836724c8
jiusui1 <1900513161@qq.com>
2024-03-10 11:06:24 +0800

    new file

---
+++ Day5/右旋转字符串.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace  std;
+
+
+
+int main(int argc, char const *argv[])
+{
+	string str;
+	int n;
+	cin >> str;
+	cin << n;
+
+	int len = str.size();
+
+	
+
+
+
+	
+	return 0;
+}
