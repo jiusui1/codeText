4d665b2bd20013e2f59b1dd5ab291bf5533f2f8e
jiusui1 <1900513161@qq.com>
2024-03-13 21:05:09 +0800

    new file

--- text.cpp
+++ text.cpp
@@ -1,25 +1,12 @@
 #include <iostream>
+using namespace std;
 
-int func(unsigned int v)
-{
-    v ^= v >> 16;
-    v ^= v >> 8;
-    v ^= v >> 2;
-    v &= 0xf;
-    return (0x6996 >> v) & 1;
-}
-int funb(int n) // 10
-{
-    unsigned int t = 1 << n;
-    int val = 0;
-    for (int i = 0; i < t; ++i) // 1024
-    {
-        val += func(i);
-    }
-    return val;
-}
-
-int main()
-{
-    funb(10);
+int main() {
+    // int n = 0;
+    // cin >> n;
+    // for (int i = 0; i < n; i++) {
+    //     cout << "hello world" << endl;
+    // }
+    cout << "system out" << endl;
+    return 0;
 }
