a7595c1d46f7f790437c78aa82e3993543de4436
jiusui1 <1900513161@qq.com>
2024-03-14 10:16:35 +0800

    new file

--- text.cpp
+++ text.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
+class B {
+   public:
+    virtual void Fun() {
+        cout << "hello" << endl;
+    }
+};
+class D : public B {
+   public:
+    void Fun() {
+        cout << "world" << endl;
+    }
+};
 
 int main() {
-    // int n = 0;
-    // cin >> n;
-    // for (int i = 0; i < n; i++) {
-    //     cout << "hello world" << endl;
-    // }
-    cout << "system out" << endl;
+    D dd;
+    B *pb = &dd;
+    D *pd = &dd;
+    pb->Fun();
+    pd->Fun();
+    // cout << "hwllo" << endl;  // D
     return 0;
 }
